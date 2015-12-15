dcbw/origin

commit 0ba1e2c2811ea9b2951bc2163fcac9db647903b7

Start SDN controller after running kubelet
On the openshift node,
 (a) SDN needs assigned subnet by openshift master
 (b) Master can only assign subnet after kubelet registers the node.
 (c) On a fresh openshift install, (a) and (b) are in deadlock as SDN will not
     proceed without subnet and kubelet will not register the node as
     it is blocked on network plugin intialization to finish.

So the fix:
 - Block SDN initialization to finish at pod setup time.
 - Run kubelet first and it registers the node if needed and
   pods may block for sdn initialization (async op).
 - Run SDN controller (doesn't need to run asynchronously) after kubelet,
   it gets the subnet, does the needed setup and marks sdn as ready.

pkg/cmd/server/start/start_node.go
@@ -262,12 +262,10 @@ func RunSDNController(config *kubernetes.NodeConfig, nodeConfig configapi.NodeCo
 	if controller != nil {
 		config.KubeletConfig.NetworkPlugins = append(config.KubeletConfig.NetworkPlugins, controller)
 
-		go func() {
-			err := controller.StartNode(nodeConfig.NetworkConfig.MTU)
-			if err != nil {
-				glog.Fatalf("SDN Node failed: %v", err)
-			}
-		}()
+		err := controller.StartNode(nodeConfig.NetworkConfig.MTU)
+		if err != nil {
+			glog.Fatalf("SDN Node failed: %v", err)
+		}
 	}
 
 	return endpointFilter
@@ -280,11 +278,11 @@ func StartNode(nodeConfig configapi.NodeConfig) error {
 	}
 	glog.Infof("Starting node %s (%s)", config.KubeletServer.HostnameOverride, version.Get().String())
 
-	endpointFilter := RunSDNController(config, nodeConfig)
 	config.EnsureVolumeDir()
 	config.EnsureDocker(docker.NewHelper())
-	config.RunProxy(endpointFilter)
 	config.RunKubelet()
+	endpointFilter := RunSDNController(config, nodeConfig)
+	config.RunProxy(endpointFilter)
 
 	return nil
 }

commit 2f7c4a3f992aea441681102572e55f65d2e9acc6

Fix race between Kubelet initialization and plugin creation
Because kubelet is started from a goroutine, sometimes it might
try to initialize network plugins before openshift-node has had
a chance to add the plugin to kubelet's list.  That causes the
openshift node process to exit with:

"Network plugin redhat/openshift-ovs-subnet not found."

Fixes: 0ba1e2c2811ea9b2951bc2163fcac9db647903b7

pkg/cmd/server/start/start_node.go
@@ -11,6 +11,7 @@ import (
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 
+	osdnapi "github.com/openshift/openshift-sdn/plugins/osdn/api"
 	"github.com/openshift/openshift-sdn/plugins/osdn/factory"
 	"github.com/openshift/origin/pkg/cmd/server/kubernetes"
 	kerrors "k8s.io/kubernetes/pkg/api/errors"
@@ -248,27 +249,20 @@ func (o NodeOptions) IsRunFromConfig() bool {
 	return (len(o.ConfigFile) > 0)
 }
 
-func RunSDNController(config *kubernetes.NodeConfig, nodeConfig configapi.NodeConfig) kubernetes.FilteringEndpointsConfigHandler {
+func InitSDNPlugin(config *kubernetes.NodeConfig, nodeConfig configapi.NodeConfig) (osdnapi.OsdnPlugin, kubernetes.FilteringEndpointsConfigHandler) {
 	oclient, _, err := configapi.GetOpenShiftClient(nodeConfig.MasterKubeConfig)
 	if err != nil {
 		glog.Fatal("Failed to get kube client for SDN")
 	}
 
-	controller, endpointFilter, err := factory.NewPlugin(nodeConfig.NetworkConfig.NetworkPluginName, oclient, config.Client, nodeConfig.NodeName, nodeConfig.NodeIP)
+	plugin, endpointFilter, err := factory.NewPlugin(nodeConfig.NetworkConfig.NetworkPluginName, oclient, config.Client, nodeConfig.NodeName, nodeConfig.NodeIP)
 	if err != nil {
 		glog.Fatalf("SDN initialization failed: %v", err)
+	} else if plugin != nil {
+		config.KubeletConfig.NetworkPlugins = append(config.KubeletConfig.NetworkPlugins, plugin)
 	}
 
-	if controller != nil {
-		config.KubeletConfig.NetworkPlugins = append(config.KubeletConfig.NetworkPlugins, controller)
-
-		err := controller.StartNode(nodeConfig.NetworkConfig.MTU)
-		if err != nil {
-			glog.Fatalf("SDN Node failed: %v", err)
-		}
-	}
-
-	return endpointFilter
+	return plugin, endpointFilter
 }
 
 func StartNode(nodeConfig configapi.NodeConfig) error {
@@ -280,8 +274,13 @@ func StartNode(nodeConfig configapi.NodeConfig) error {
 
 	config.EnsureVolumeDir()
 	config.EnsureDocker(docker.NewHelper())
+	plugin, endpointFilter := InitSDNPlugin(config, nodeConfig)
 	config.RunKubelet()
-	endpointFilter := RunSDNController(config, nodeConfig)
+	if plugin != nil {
+		if err := plugin.StartNode(nodeConfig.NetworkConfig.MTU); err != nil {
+			glog.Fatalf("SDN Node failed: %v", err)
+		}
+	}
 	config.RunProxy(endpointFilter)
 
 	return nil